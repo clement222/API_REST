8bd7debbdf492451572d697b576ba862341ec033
OX <ox@MacBook-Pro-de-OX.local>
2016-11-28 21:05:14 +0100

    mise en place template

---
+++ get.php
@@ -0,0 +1,146 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>Money Push API</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="Site web pour utiliser une API REST" />
+  <meta name="keywords" content="TP API" />
+
+  <!-- Facebook and Twitter integration -->
+  <meta property="og:title" content="" />
+  <meta property="og:image" content="" />
+  <meta property="og:url" content="" />
+  <meta property="og:site_name" content="" />
+  <meta property="og:description" content="" />
+  <meta name="twitter:title" content="" />
+  <meta name="twitter:image" content="" />
+  <meta name="twitter:url" content="" />
+  <meta name="twitter:card" content="" />
+
+  <link href='https://fonts.googleapis.com/css?family=Work+Sans:400,300,600,400italic,700' rel='stylesheet' type='text/css'>
+  <link href="https://fonts.googleapis.com/css?family=Playfair+Display:400,700" rel="stylesheet">
+
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css/icomoon.css">
+  <!-- Bootstrap  -->
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <!-- Theme style  -->
+  <link rel="stylesheet" href="css/style.css">
+
+  <!-- Modernizr JS -->
+  <script src="js/modernizr-2.6.2.min.js"></script>
+  <!-- FOR IE9 below -->
+  <!--[if lt IE 9]>
+  <script src="js/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+
+<div class="fh5co-loader"></div>
+
+<div id="page">
+  <nav class="fh5co-nav" role="navigation">
+    <div class="container">
+      <div class="row">
+        <div class="left-menu text-right menu-1">
+          <ul>
+            <li><a href="add.html">Ajouter</a></li>
+            <li class="active"><a href="get.php">Récupérer</a></li>
+          </ul>
+        </div>
+        <div class="logo text-center">
+          <div id="fh5co-logo"><a href="index.html">Money Push</a></div>
+        </div>
+        <div class="right-menu text-left menu-1">
+          <ul>
+            <li><a href="put.html">Modifier</a></li>
+            <li><a href="delete.html">Supprimer</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </nav>
+
+  <header id="fh5co-header" class="fh5co-cover" role="banner" style="height: 180px;">
+    <div class="overlay"></div>
+  </header>
+
+  <div id="fh5co-project">
+    <div class="container">
+      <div class="row animate-box">
+        <div class="col-md-8 col-md-offset-2 text-center fh5co-heading">
+          <h2>Les ordres</h2>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-1.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Sea</h3>
+            <span>Web Design</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-2.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Nature</h3>
+            <span>Application</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-3.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Green Island</h3>
+            <span>Branding</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-4.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Workspace</h3>
+            <span>Web Design</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-5.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Nature</h3>
+            <span>Application</span>
+          </a>
+        </div>
+        <div class="col-md-4 col-sm-6 fh5co-project animate-box" data-animate-effect="fadeIn">
+          <a href="#"><img src="images/work-6.jpg" alt="Free HTML5 Website Template by FreeHTML5.co" class="img-responsive">
+
+            <h3>Green Island</h3>
+            <span>Branding</span>
+          </a>
+        </div>
+
+      </div>
+    </div>
+  </div>
+
+</div>
+
+<div class="gototop js-top">
+  <a href="#" class="js-gotop"><i class="icon-arrow-up"></i></a>
+</div>
+
+<!-- jQuery -->
+<script src="js/jquery.min.js"></script>
+<!-- jQuery Easing -->
+<script src="js/jquery.easing.1.3.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.min.js"></script>
+<!-- Waypoints -->
+<script src="js/jquery.waypoints.min.js"></script>
+<!-- Main -->
+<script src="js/main.js"></script>
+
+</body>
+</html>
+
